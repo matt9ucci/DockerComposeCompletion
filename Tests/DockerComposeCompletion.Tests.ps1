f342ab0fb3aab778400520eda3d7d39fe21af8d7
Matt Gucci <matt9ucci@gmail.com>
2019-02-04 20:35:58 +0900

    Add Tests

---
+++ Tests/DockerComposeCompletion.Tests.ps1
@@ -0,0 +1,30 @@
+Import-Module -Name $PSScriptRoot\..\DockerComposeCompletion -Force
+
+function Get-Result([string]$Text, [int]$CursorPosition = $Text.Length, [hashtable]$Options) {
+	[System.Management.Automation.CommandCompletion]::CompleteInput($Text, $CursorPosition, $Options).CompletionMatches
+}
+
+Describe 'docker-compose' {
+	$result = Get-Result 'docker-compose --t'
+	$result.Count | Should Be 5
+	$result[0].CompletionText | Should Be --tls
+	$result[1].CompletionText | Should Be --tlscacert
+	$result[2].CompletionText | Should Be --tlscert
+	$result[3].CompletionText | Should Be --tlskey
+	$result[4].CompletionText | Should Be --tlsverify
+
+	$result = Get-Result 'docker-compose bu'
+	$result.Count | Should Be 2
+	$result[0].CompletionText | Should Be build
+	$result[1].CompletionText | Should Be bundle
+}
+
+Describe 'docker-compose up' {
+	$result = Get-Result 'docker-compose up --no-'
+	$result.Count | Should Be 5
+	$result[0].CompletionText | Should Be --no-build
+	$result[1].CompletionText | Should Be --no-color
+	$result[2].CompletionText | Should Be --no-deps
+	$result[3].CompletionText | Should Be --no-recreate
+	$result[4].CompletionText | Should Be --no-start
+}
