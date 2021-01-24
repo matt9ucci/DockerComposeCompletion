32551e0a75cb8cf35bd96c554ac45a035280812a
Matt Gucci <matt9ucci@gmail.com>
2021-01-24 21:10:26 +0900

    Use Pester 5

--- Tests/DockerComposeCompletion.Tests.ps1
+++ Tests/DockerComposeCompletion.Tests.ps1
@@ -1,29 +1,39 @@
-Import-Module -Name $PSScriptRoot\..\DockerComposeCompletion -Force
+#Requires -Module @{ ModuleName = 'Pester'; ModuleVersion = '5.1.1' }
 
-function Get-Result([string]$Text, [int]$CursorPosition = $Text.Length, [hashtable]$Options) {
-	[System.Management.Automation.CommandCompletion]::CompleteInput($Text, $CursorPosition, $Options).CompletionMatches
+BeforeAll {
+	Import-Module $PSScriptRoot/../DockerComposeCompletion -Force
+
+	function Get-Result([string]$Text, [int]$CursorPosition = $Text.Length, [hashtable]$Options) {
+		[System.Management.Automation.CommandCompletion]::CompleteInput($Text, $CursorPosition, $Options).CompletionMatches
+	}
 }
 
-Describe 'docker-compose' {
-	$result = Get-Result 'docker-compose --t'
-	$result.Count | Should Be 5
-	$result[0].CompletionText | Should Be --tls
-	$result[1].CompletionText | Should Be --tlscacert
-	$result[2].CompletionText | Should Be --tlscert
-	$result[3].CompletionText | Should Be --tlskey
-	$result[4].CompletionText | Should Be --tlsverify
+Describe "docker-compose" {
+	It "completes --t" {
+		$result = Get-Result 'docker-compose --t'
+		$result | Should -HaveCount 5
+		$result[0].CompletionText | Should -Be --tls
+		$result[1].CompletionText | Should -Be --tlscacert
+		$result[2].CompletionText | Should -Be --tlscert
+		$result[3].CompletionText | Should -Be --tlskey
+		$result[4].CompletionText | Should -Be --tlsverify
+	}
 
-	$result = Get-Result 'docker-compose bu'
-	$result.Count | Should Be 1
-	$result[0].CompletionText | Should Be build
+	It "completes bu" {
+		$result = Get-Result 'docker-compose bu'
+		$result | Should -HaveCount 1
+		$result[0].CompletionText | Should -Be build
+	}
 }
 
 Describe 'docker-compose up' {
-	$result = Get-Result 'docker-compose up --no-'
-	$result.Count | Should Be 5
-	$result[0].CompletionText | Should Be --no-build
-	$result[1].CompletionText | Should Be --no-color
-	$result[2].CompletionText | Should Be --no-deps
-	$result[3].CompletionText | Should Be --no-recreate
-	$result[4].CompletionText | Should Be --no-start
+	It "completes up --no-" {
+		$result = Get-Result 'docker-compose up --no-'
+		$result | Should -HaveCount 5
+		$result[0].CompletionText | Should -Be --no-build
+		$result[1].CompletionText | Should -Be --no-color
+		$result[2].CompletionText | Should -Be --no-deps
+		$result[3].CompletionText | Should -Be --no-recreate
+		$result[4].CompletionText | Should -Be --no-start
+	}
 }
